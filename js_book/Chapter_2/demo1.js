4b54e70fdbea77cf02a95d488ec4796149202fd6
jmbriody <jackbriody@gmail.com>
2019-03-19 21:27:39 -0500

    JS configurations

--- js_book/Chapter_2/demo1.js
+++ js_book/Chapter_2/demo1.js
@@ -1,10 +1,16 @@
 ﻿'use strict';
+const _ = require('underscore');
 
 const sentences = [
     { subject: 'JavaScript', verb: 'is', object: 'great' },
     { subject: 'Elephants', verb: 'are', object: 'large' },
 ];
 
+const verbs = _.pluck(sentences, 'verb');
+
+for(let verb of verbs) {
+    console.log(`found verb: ${verb}`);
+}
 function say({ subject, verb, object }) {
     console.log(`${subject} ${verb} ${object}`);
 }
